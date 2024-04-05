02e19a32c8c93b4f56fee46977c6f3d317f79694
fmahe <francois.mahe@ens-rennes.fr>
2024-04-05 12:07:09 +0200

    Update test_solver.py

--- test/test_solver.py
+++ test/test_solver.py
@@ -26,8 +26,10 @@ def test_solver():
 
     # Solve the mechanical packing problem
     K, C, u, f, F, H, Z, rlambda, mask, err = solve(
-        mat, mesh, packing=4, itermax=1000,
-        lmin=0.01, coupling=0.99, interp_size=100
+        mesh,
+        stiffness(mat, mesh),
+        constraint(mat, mesh),
+        packing=4,
     )
 
     assert np.allclose(
@@ -68,8 +70,10 @@ if __name__ == '__main__':
 
     # Solve the mechanical packing problem
     K, C, u, f, F, H, Z, rlambda, mask, err = solve(
-        mat, mesh, packing=4, itermax=1000,
-        lmin=0.01, coupling=0.99, interp_size=100
+        mesh,
+        stiffness(mat, mesh),
+        constraint(mat, mesh),
+        packing=4,
     )
 
     # Deform the mesh
